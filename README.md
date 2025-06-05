408d3b56c05a109217648223d489e01b0fdf33db
skyaironline <aldo@batamon.asia>
2025-06-05 23:10:12 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# What are we making
+- Just some minital HTML/JS site that has ability to map to solidity smart contract for 
+    - Connect
+    - Buy Me a Coffee
+    - Get Balance
+    - Withdraw
---
+++ index-js.js
@@ -0,0 +1,19 @@
+import {createWalletClient, custom} from "https://esm.sh/viem"
+
+const connectButton = document.getElementById('connectButton')
+
+let walletClient
+
+async function connect() {
+    if(typeof window.ethereum !== 'undefined') {
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum)
+        })
+        await walletClient.requestAddresses();
+    } else {
+        connectButton.innerText = 'Please install MetaMask'
+    }
+}
+
+connectButton.onclick = connect
+
