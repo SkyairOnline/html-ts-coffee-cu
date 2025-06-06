e2f4b658f717b7607dbcfe1abc35c076cf68530a
skyaironline <aldo@batamon.asia>
2025-06-06 20:11:44 +0700

    implement get balance function

--- index-js.js
+++ index-js.js
@@ -1,15 +1,16 @@
-import {createWalletClient, custom, createPublicClient, parseEther, defineChain } from "https://esm.sh/viem"
+import { createWalletClient, custom, createPublicClient, parseEther, defineChain, formatEther } from "https://esm.sh/viem"
 import { contractAddress, coffeeAbi } from "./constants-js.js"
 
 const connectButton = document.getElementById('connectButton')
 const fundButton = document.getElementById('fundButton')
 const ethAmountInput = document.getElementById('ethAmount')
+const balanceButton = document.getElementById('balanceButton')
 
 let walletClient
 let publicClient
 
 async function connect() {
-    if(typeof window.ethereum !== 'undefined') {
+    if (typeof window.ethereum !== 'undefined') {
         walletClient = createWalletClient({
             transport: custom(window.ethereum)
         })
@@ -23,7 +24,7 @@ async function fund() {
     const ethAmount = ethAmountInput.value;
     console.log(`Funding with ${ethAmount} ETH`);
 
-    if(typeof window.ethereum !== 'undefined') {
+    if (typeof window.ethereum !== 'undefined') {
         walletClient = createWalletClient({
             transport: custom(window.ethereum)
         })
@@ -33,7 +34,7 @@ async function fund() {
         publicClient = createPublicClient({
             transport: custom(window.ethereum)
         })
-        const {request} = await publicClient.simulateContract({
+        const { request } = await publicClient.simulateContract({
             address: contractAddress,
             abi: coffeeAbi,
             functionName: 'fund',
@@ -69,6 +70,18 @@ async function getCurrentChain(client) {
     return currentChain
 }
 
+async function getBalance() {
+    if (typeof window.ethereum !== 'undefined') {
+        publicClient = createPublicClient({
+            transport: custom(window.ethereum)
+        })
+        const balance = await publicClient.getBalance({
+            address: contractAddress
+        })
+        console.log(`Contract balance: ${formatEther(balance)} ETH`);
+    } 
+}
+
 connectButton.onclick = connect
 fundButton.onclick = fund
-
+balanceButton.onclick = getBalance
